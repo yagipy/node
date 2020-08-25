c06ae0b4f941c480171c71fd3df4f3c0622caf23
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-08-25 08:55:13 +0200

    test: update error code for OpenSSL3.x

--- test/parallel/test-tls-key-mismatch.js
+++ test/parallel/test-tls-key-mismatch.js
@@ -30,7 +30,7 @@ const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const tls = require('tls');
 const errorMessageRegex = common.hasOpenSSL3 ?
-  /^Error: error:0B000074:x509 certificate routines::key values mismatch$/ :
+  /^Error: error:05800074:x509 certificate routines::key values mismatch$/ :
   /^Error: error:0B080074:x509 certificate routines:X509_check_private_key:key values mismatch$/;
 
 const options = {
